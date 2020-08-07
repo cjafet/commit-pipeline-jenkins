b636dadc1615e9676fe7b8e096b9c98dc30e0f5f
Carlos Jafet Neto <carlos.neto@zup.com.br>
2020-08-07 11:35:23 -0300

    Add git.sh file

---
+++ policy/git.sh
@@ -0,0 +1,11 @@
+# Copy an empty repo
+git clone --no-checkout https://github.com/cjafet/commit-pipeline-jenkins.git
+
+# Move into the empty repo
+cd policy
+
+# Initialize sparse-checkout
+git sparse-checkout init --cone
+
+# Set/Checkout folder
+git sparse-checkout set policy
