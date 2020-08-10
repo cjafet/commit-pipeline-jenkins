0e8c8ec1dc70ae4bc4b873d9e7af7440e6fedf5c
Carlos Jafet Neto <carlos.neto@zup.com.br>
2020-08-10 12:32:20 -0300

    Fix files

--- git-policy.sh
+++ git-policy.sh
@@ -1,5 +1,10 @@
+#!/bin/sh
+
 # Copy an empty repo 
 git clone --no-checkout https://github.com/cjafet/commit-pipeline-jenkins.git
+
+# Move into the cloned directory
+cd commit-pipeline-jenkins
 
 # Set/Checkout folder
 git sparse-checkout set policy
--- git.sh
+++ git.sh
@@ -2,9 +2,10 @@
 git clone --no-checkout https://github.com/cjafet/commit-pipeline-jenkins.git
 
 # Move into the policy directory
-cd policy
+cd commit-pipeline-jenkins
 
 # Initialize sparse-checkout
+# Clone files in the root 
 git sparse-checkout init --cone
 
 # Set/Checkout folder
