5ca30f7e3f059c8954d71a7eb3c74fe80680dac2
Carlos Jafet Neto <carlos.neto@zup.com.br>
2020-08-10 11:37:05 -0300

    Change git file

--- git.sh
+++ git.sh
@@ -1,10 +1,8 @@
-rm -rf 
-
 # Copy an empty repo 
 git clone --no-checkout https://github.com/cjafet/commit-pipeline-jenkins.git .
 
 # Move into the policy directory
-cd commit-pipeline-jenkins/policy
+cd policy
 
 # Initialize sparse-checkout
 git sparse-checkout init --cone
