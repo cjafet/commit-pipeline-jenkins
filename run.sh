f31eec699aa7418f63f3c2c5f0a069b47044adb4
Carlos Jafet Neto <carlos.neto@zup.com.br>
2020-08-13 18:11:38 -0300

    Fix namespace

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ query=$(jq '.regoQueryName' config-policy.json | sed 's/\"//g')
 
 payload='{
     "configMapName": '\"${namespace}.${query}\"',
-    "namespace": '\"${query}\"',
+    "namespace": '\"${namespace}\"',
     "data": {
           "data.json": '${datajson}',
           "config-policy.json": '${configpolicy}',
@@ -19,4 +19,4 @@ payload='{
 
 echo $payload
 
-#curl -H "Content-Type: application/json" -X PUT -d "$payload" http://aa736da781a634ef7986805596c267f3-403110847.sa-east-1.elb.amazonaws.com/policy
+curl -H "Content-Type: application/json" -X PUT -d "$payload" http://aa736da781a634ef7986805596c267f3-403110847.sa-east-1.elb.amazonaws.com/policy
