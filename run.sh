0672bc67e226fc6a5dacbcf6458e1b669442c683
Carlos Jafet Neto <carlos.neto@zup.com.br>
2020-08-13 11:01:53 -0300

    Add new police file

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 j=`cat config.json`
 echo $j
-curl -H "Content-Type: application/json" -X PUT -d "$j" https://postman-echo.com/put
+curl -H "Content-Type: application/json" -X PUT -d "$j" http://aa736da781a634ef7986805596c267f3-403110847.sa-east-1.elb.amazonaws.com/policy
