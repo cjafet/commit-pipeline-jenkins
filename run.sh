692d3a164a57d3115dbeff7f4a5a9830a0e80276
Carlos Jafet Neto <carlos.neto@zup.com.br>
2020-08-13 16:47:19 -0300

    Fix policy payload

--- run.sh
+++ run.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
+datajson=`cat policy/data.json`
+validateprofile=`cat policy/validate-profile.rego`
+configpolicy=`cat policy/config-policy.json`
 
-j=`cat config.json`
-echo $j
-curl -H "Content-Type: application/json" -X PUT -d "$j" http://aa736da781a634ef7986805596c267f3-403110847.sa-east-1.elb.amazonaws.com/policy
+namespace=echo jq '.namespace' policy/config-policy.json | sed 's/\"//'
+query=echo jq '.regoQueryName' policy/config-policy.json | sed 's/\"//'
+
+
+payload='{
+    "configMapName": '"${namespace}.${query}"',
+    "namespace": '"${query}"',
+    "data": {
+          "data.json": '${datajson}',
+          "config-policy.json": '${configpolicy}',
+          "validate-profile.rego": '${validateprofile}'
+        }
+}'
+
+echo $payload
+
+curl -H "Content-Type: application/json" -X PUT -d "$payload" http://aa736da781a634ef7986805596c267f3-403110847.sa-east-1.elb.amazonaws.com/policy
